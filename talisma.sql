a7bd4d286e81377a290e811545f3cbbb48b1a4f6
danielvramos <vieira.ramos@gmail.com>
2021-06-14 19:42:37 -0300

    banco de dados

--- talisma.sql
+++ talisma.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- Servidor:                     127.0.0.1
--- Versão do servidor:           5.7.33 - MySQL Community Server (GPL)
+-- Versão do servidor:           8.0.23 - MySQL Community Server - GPL
 -- OS do Servidor:               Win64
 -- HeidiSQL Versão:              11.2.0.6213
 -- --------------------------------------------------------
@@ -12,87 +12,55 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
--- Copiando estrutura do banco de dados para talisma
-CREATE DATABASE IF NOT EXISTS `talisma` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `talisma`;
-
 -- Copiando estrutura para tabela talisma.contatos
 CREATE TABLE IF NOT EXISTS `contatos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_fornecedor` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `fone` varchar(20) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `fone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `idFornecedor` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_id_fornecedor` (`id_fornecedor`),
-  CONSTRAINT `fk_id_fornecedor` FOREIGN KEY (`id_fornecedor`) REFERENCES `fornecedores` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+  KEY `FK_contato_fornecedor` (`idFornecedor`),
+  CONSTRAINT `FK_contato_fornecedor` FOREIGN KEY (`idFornecedor`) REFERENCES `fornecedores` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Copiando dados para a tabela talisma.contatos: ~1 rows (aproximadamente)
-/*!40000 ALTER TABLE `contatos` DISABLE KEYS */;
-INSERT INTO `contatos` (`id`, `id_fornecedor`, `nome`, `email`, `fone`) VALUES
-	(6, 1, 'FULANO', 'fulano@gmail.com', '2199999999');
-/*!40000 ALTER TABLE `contatos` ENABLE KEYS */;
+-- Exportação de dados foi desmarcado.
 
 -- Copiando estrutura para tabela talisma.cotacaofornecedores
 CREATE TABLE IF NOT EXISTS `cotacaofornecedores` (
-  `idProduto` int(11) DEFAULT NULL,
-  `idFornecedor` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `idProduto` int NOT NULL,
+  `idFornecedor` int NOT NULL,
+  PRIMARY KEY (`idProduto`,`idFornecedor`),
+  KEY `FK_fornecedor` (`idFornecedor`),
+  KEY `FK_produto` (`idProduto`) USING BTREE,
+  CONSTRAINT `FK_fornecedor` FOREIGN KEY (`idFornecedor`) REFERENCES `fornecedores` (`id`),
+  CONSTRAINT `FK_produto` FOREIGN KEY (`idProduto`) REFERENCES `cotacaoprodutos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Copiando dados para a tabela talisma.cotacaofornecedores: ~3 rows (aproximadamente)
-/*!40000 ALTER TABLE `cotacaofornecedores` DISABLE KEYS */;
-INSERT INTO `cotacaofornecedores` (`idProduto`, `idFornecedor`) VALUES
-	(2, 2),
-	(3, 2),
-	(2, 3);
-/*!40000 ALTER TABLE `cotacaofornecedores` ENABLE KEYS */;
+-- Exportação de dados foi desmarcado.
 
 -- Copiando estrutura para tabela talisma.cotacaoprodutos
 CREATE TABLE IF NOT EXISTS `cotacaoprodutos` (
-  `id` int(11) DEFAULT NULL,
-  `nome` varchar(50) DEFAULT NULL,
-  `categoria` varchar(50) DEFAULT NULL,
-  `valor` int(11) DEFAULT NULL,
-  `dataValidade` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `categoria` varchar(50) NOT NULL,
+  `valor` double NOT NULL,
+  `dataValidade` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Copiando dados para a tabela talisma.cotacaoprodutos: ~3 rows (aproximadamente)
-/*!40000 ALTER TABLE `cotacaoprodutos` DISABLE KEYS */;
-INSERT INTO `cotacaoprodutos` (`id`, `nome`, `categoria`, `valor`, `dataValidade`) VALUES
-	(1, 'teste', 'teste', 1250, 2021),
-	(2, 'teste', 'teste', 1000, 2021),
-	(3, 'teste', 'teste', 600, 2021);
-/*!40000 ALTER TABLE `cotacaoprodutos` ENABLE KEYS */;
+-- Exportação de dados foi desmarcado.
 
 -- Copiando estrutura para tabela talisma.fornecedores
 CREATE TABLE IF NOT EXISTS `fornecedores` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cnpj` varchar(50) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cnpj` varchar(14) NOT NULL,
   `nomeFantasia` varchar(50) NOT NULL,
   `razaoSocial` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Copiando dados para a tabela talisma.fornecedores: ~3 rows (aproximadamente)
-/*!40000 ALTER TABLE `fornecedores` DISABLE KEYS */;
-INSERT INTO `fornecedores` (`id`, `cnpj`, `nomeFantasia`, `razaoSocial`) VALUES
-	(1, '45678912540', 'SAMSUNG ELECTRONICS', 'SAMSUNG ELECTRONICS CO. LTD.'),
-	(2, '65478215634859', 'MOTOROLA', 'MOTOROLA MOBILITY PRODUTOS ELETRONICOS LTDA'),
-	(3, '64125873254103', 'PONTO FRIO', ' VIA VAREJO S/A');
-/*!40000 ALTER TABLE `fornecedores` ENABLE KEYS */;
-
--- Copiando estrutura para tabela talisma.produtos
-CREATE TABLE IF NOT EXISTS `produtos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Copiando dados para a tabela talisma.produtos: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+-- Exportação de dados foi desmarcado.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
